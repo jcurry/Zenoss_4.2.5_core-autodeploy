a3cbd460a40d6fa6aa7c0dbc36d4c31c3f42fa37
Jane Curry <jane.curry@skills-1st.co.uk>
2016-12-09 17:53:01 +0000

    Zenoss rpm location changed

--- zo425_ubuntu-debian.sh
+++ zo425_ubuntu-debian.sh
@@ -85,7 +85,9 @@ if [ $curos = "debian" ]; then
 fi
 
 # Download Zenoss DEB and install it
-wget -N http://softlayer-dal.dl.sourceforge.net/project/zenossforubuntu/zenoss-core-425-2108_03c_amd64.deb -P $DOWNDIR/
+# Location for Zenoss has changed....
+#wget -N http://softlayer-dal.dl.sourceforge.net/project/zenossforubuntu/zenoss-core-425-2108_03c_amd64.deb -P $DOWNDIR/
+wget -N https://sourceforge.net/projects/zenossforubuntu/files/zenoss-core-425-2108_03c_amd64.deb/download -P $DOWNDIR/
 if [ $UPGRADE = "no" ]; then
 	dpkg -i $DOWNDIR/zenoss-core-425-2108_03c_amd64.deb
 fi
